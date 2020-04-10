54975c06990cbc9ba1e90b2a426553735a1bb29a
jhryu1208 <jhryu1208@gmail.com>
2020-04-11 08:17:49 +0900

    JH_PYTHON_CHAPTER1

--- Basic Grammar/Chpater 01_01.py
+++ Basic Grammar/Chpater 01_01.py
@@ -6,6 +6,7 @@
 # 작은 따옴표 - 주로 사용하는 것은 작옴따옴표 한쌍
 print('Python Start!')
 print('''Python Start!''')
+
 # 큰 따옴표
 print("Python Start!")
 print("""Python Start!""")
@@ -53,3 +54,60 @@ print('{} {}'.format('one','two'))
 # 따라서 다음과같이 {1} {0} 으로 할 경우, format함수에서 1번째 값이 먼저 출력되고 0번쨰값이 나중에 출력된다.
 # 결론적으로, 아래와 같은 방식으로 순서를 지정해서 출력할 수 있다.
 print('{1} {0}'.format('one','two'))
+
+print()
+
+# [%s]
+# _ _ _ _ _ _ n i c e
+print('%10s'%('nice'))
+# 10자리를 확보하고 nice로 format
+print('{:>10}'.format('nice'))
+
+# n i c e _ _ _ _ _ _
+print('%-10s'%('nice'))
+print('{:10}'.format('nice'))
+
+# >왼쪽의 기호를 문자열을 제외한 공백에 출력한다
+# ______nice
+print('{:_>10}'.format('nice'))
+
+# :^는 중앙정렬
+print('{:^10}'.format('nice'))
+
+# %.숫자 : 원하는 자릿수만큼 출력
+print('%.5s' % ('nice'))
+print('%.5s' % ('pythonstudy'))
+print('%5s' % ('pythonstudy'))
+# 공간은 10개가 있고, 왼쪽부터 5개만 출력한다.
+print ("{:10.5}".format('pythonstudy'))
+
+print()
+
+# [%d]
+print ('%d %d'%(1,2))
+print ('{} {}'.format(1,2))
+
+# _ _ 4 2
+print ('%4d'%(42))
+print ('{:4d}'.format(42))
+
+print()
+
+# [%f]
+# %정수부.소수부f
+# 전부다 있는 그대로 출력x
+print('%f' % (3.141521341231412412))
+# :f를 안쓸경우 모든 수가 출력된다.
+print('{:f}'.format(3.141521341231412412))
+
+# 정수부 한자리, 소수부 8자리
+print('%1.8f' % (3.141521341231412412))
+# 정수부 한자리, 소수부 18자리
+print('%1.18f' % (3.141521341231412412))
+
+# _ _ _ _ _ _
+# 0 0 3 . 1 4 , 총 6자리 중에서 소숫점 2자리까지
+print('%06.2f'%(3.141521341231412412))
+print('{:06.2f}'.format(3.141521341231412412))
+
+print()

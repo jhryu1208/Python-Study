643f60c912142e686f48d08d9641cd20623920f9
jhryu1208 <jhryu1208@gmail.com>
2020-07-11 02:57:46 +0900

    JH_python_study

--- Basic Grammar/Chapter 10_1.py
+++ Basic Grammar/Chapter 10_1.py
@@ -30,22 +30,24 @@ turns = 10
 # 찬스 카운트가 남아 있을 경우
 while turns  > 0:
     # 실패 횟수(단어 매치 수)
-    failed = 0
+    failed = 0                                                  ## fail을 0으로 지속적으로 초기화 시켜준다.
 
     # 정답 단어 반복
-    for char in word:
+    for char in word:                                           ## word의 secret의 단어가 6글자기 때문에 6번 반복된다.
         # 정답 단어 내에 추측 문자가 포함되어 있는 경우
-        if char in guesses:
+        if char in guesses:                                     ## (첫 순서) guesses는 공백이기 때문에 실행되지 않는다.
             # 추측 단어 출력
             print(char, end = ' ')
         else:
             # 틀린 경우 대시로 처리
-            print("_", end = ' ')
-            failed += 1
+            print("_", end = ' ')                               ## (첫 순서) if문 실행되지 않았기 때문에 else문이 6번 반복 실행된다.
+            failed += 1                                         ## (첫 순서) 6번 반복 실행되었기 때문에, failed에는 6이 적립되어있다.
+
+
     # 단어 추측이 성공한 경우
-    if failed == 0:
-        print()
-        print()
+    if failed == 0:                                             ## (첫 순서) failed는 6이 적립되어있기 때문에, 이 if문은 실행되지 않는다.
+        print()                                                 ## (맞출때) guesses에서는 텍스트가 지속적으로 누적된다.
+        print()                                                 ##          따라서, 누적된 텍스트가 success를 전부 포함할 경우 failed는 0이다.
         print('Congratulation! The Guesses is correct.')
         # while 구문 중단
         break
@@ -56,7 +58,7 @@ while turns  > 0:
     guess = input("guess a character.")
 
     # 단어 더하기
-    guesses += guess
+    guesses += guess                                            ## 계속 텍스트가 누적된다!
 
     # 정답 단어에 추측한 문자가 포함되어에 있지 않으면
     if guess not in word:
